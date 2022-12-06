9c63af7ba50d371d0c9eb3bc01d48c805fbfb4af
lcg <lcg635@126.com>
2022-12-06 21:21:39 +0800

    update mappings

--- mappings.lua
+++ mappings.lua
@@ -1,29 +1,41 @@
 return {
   -- first key is the mode
   n = {
-    ["<Leader>d"] = false,
+    ["<leader>d"] = false,
+    ["<leader>c"] = false,
+
     ["<cr>"] = { "zt", desc = "Top The Line" },
-    [",w"] = { ":w<cr>", desc = "Save" },
     -- second key is the lefthand side of the map
+    ["<leader>."] = { "<cmd>cd %:p:h<cr>", desc = "Set CWD" },
+
     -- mappings seen under group name "Buffer"
+    ["<leader>q"] = { ":Bdelete!<cr>", desc = "Quit Buffer" },
     ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
     ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
     ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
     ["<leader>bl"] = { "<cmd>BufferLineCloseRight<cr>", desc = "Close Right" },
     ["<leader>bh"] = { "<cmd>BufferLineCloseLeft<cr>", desc = "Close Left" },
+
+    ["<leader>cw"] = {
+      function()
+        local word = vim.fn.expand "<cword>"
+        local rp = vim.fn.input "Replace with: "
+        vim.cmd("%s/" .. word .. "/" .. rp .. "/g")
+      end,
+    },
+
     ["\\"] = { "<cmd>split<cr>", desc = "Horizontal split" },
     ["|"] = { "<cmd>vsplit<cr>", desc = "Vertical split" },
 
-    ["<F4>"] = { ":lua require('dap').toggle_breakpoint()<CR>", desc = "Toggle Breakpoint" },
+    ["<F5>"] = { ":lua require('dap').toggle_breakpoint()<CR>", desc = "Toggle Breakpoint" },
     ["<F9>"] = { ":lua require('dap').continue()<CR>", desc = "Debug Continue" },
-    ["<F5>"] = { ":lua require('dap').step_over()<CR>", desc = "Debug Step Over" },
-    ["<F6>"] = { ":lua require('dap').step_into()<CR>", desc = "Debug Step Into" },
-    ["<F7>"] = { ":lua require('dap').step_out()<CR>", desc = "Debug Step Out" },
+    ["<F10>"] = { ":lua require('dap').step_over()<CR>", desc = "Debug Step Over" },
+    ["<F11>"] = { ":lua require('dap').step_into()<CR>", desc = "Debug Step Into" },
+    ["<F12>"] = { ":lua require('dap').step_out()<CR>", desc = "Debug Step Out" },
     ["<Leader>dh"] = { ":lua require('dap.ui.variables').hover()<CR>", desc = "Debug Hover" },
   },
   i = {
-    [",w"] = { "<esc>:w<cr>", desc = "Save" },
   },
   v = {
     ["<Leader>dv"] = { ":lua require('dap.ui.variables').visual_hover()<CR>", desc = "Debug Hover" },
