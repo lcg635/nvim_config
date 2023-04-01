9031ff64cf03663d0a8b97bbe94964e1f466b7a7
lcg <lcg635@126.com>
2023-04-01 15:12:28 +0800

    update

--- mappings.lua
+++ mappings.lua
@@ -3,24 +3,24 @@ return {
   n = {
     L = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
     H = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer" },
+    T = { "zt", desc = "Top The Line" },
+    B = { "zb", desc = "Bottom The Line" },
+    ["\\"] = { "<cmd>split<cr>", desc = "Horizontal split" },
+    ["|"] = { "<cmd>vsplit<cr>", desc = "Vertical split" },
+    ["<cr>"] = { "zz", desc = "Center The Line" },
 
-    ["Q"] = { "<cmd>qa<cr>", desc = "Quit all" },
-    -- ["<c-D>"] = { "yyp", desc = "Copy line" },
-
-    ["<a-/>"] = { function() require("Comment.api").toggle.linewise.current() end, desc = "Comment line" },
-    ["<cr>"] = { "zz", desc = "Top The Line" },
     [",w"] = { "<esc>:w<cr>", desc = "Save" },
+    [",q"] = { "<cmd>qa<cr>", desc = "Quit all" },
+    [",d"] = { "yyp", desc = "Copy line" },
+    [",r"] = { ':%s/<c-r><c-w>/', desc = "Replace current word"},
+    [",R"] = { '<cmd>e!<cr>', desc = "Refresh buffers"},
+    [",c"] = { function() require("Comment.api").toggle.linewise.current() end, desc = "Comment line" },
 
     ["<F7>"] = false,
     ["<F11>"] = { "<cmd>1ToggleTerm<cr>", desc = "Toggle terminal" },
     ["<F12>"] = { "<cmd>2ToggleTerm size=11 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" },
-    ["\\"] = { "<cmd>split<cr>", desc = "Horizontal split" },
-    ["|"] = { "<cmd>vsplit<cr>", desc = "Vertical split" },
 
     ["<leader>."] = { "<cmd>cd %:p:h<cr>", desc = "Set CWD" },
-    ["<leader>r"] = { ':%s/<c-r><c-w>/', desc = "Replace current word"},
-    ["<leader>R"] = { '<cmd>e!<cr>', desc = "Refresh buffers"},
-
     ["<leader>fp"] = { "<cmd>Telescop projects<cr>", desc = "Find projects" },
   },
   i = {
@@ -31,7 +31,7 @@ return {
   },
   v = {
     -- ["<leader>dv"] = { ":lua require('dap.ui.variables').visual_hover()<CR>", desc = "Debug Hover" },
-    ["<a-/>"] = { "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", desc = "Comment" },
+    [",c"] = { "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", desc = "Comment" },
   },
   t = {
     -- setting a mapping to false will disable it
