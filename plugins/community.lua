54b94cc803da51851cc337bdcdc7194c41e58e25
lcg <lcg635@126.com>
2023-04-03 00:20:42 +0800

    add typescript plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
     { import = "astrocommunity.completion.copilot-lua-cmp" },
     { import = "astrocommunity/debugging/nvim-bqf" },
     { import = "astrocommunity/pack/go" },
+    { import = "astrocommunity/pack/typescript" },
     { import = "astrocommunity/project/project-nvim" }
     -- { import = "astrocommunity/scrolling/mini-animate" }
 }
