5eebbae24ca6043fed0a63f8f03fe7e67142549e
lcg <lcg635@126.com>
2023-04-03 00:15:53 +0800

    add go plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,7 @@ return {
     { import = "astrocommunity.colorscheme.kanagawa" },
     { import = "astrocommunity.completion.copilot-lua-cmp" },
     { import = "astrocommunity/debugging/nvim-bqf" },
+    { import = "astrocommunity/pack/go" },
     { import = "astrocommunity/project/project-nvim" }
     -- { import = "astrocommunity/scrolling/mini-animate" }
 }
