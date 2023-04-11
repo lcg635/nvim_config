62a5b4904f9ef5bb2eb615597322a9e070f92afd
lcg <lcg635@126.com>
2023-04-11 20:29:15 +0800

    update

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,7 +2,22 @@ return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.kanagawa" },
     { import = "astrocommunity/colorscheme/rose-pine" },
-    { import = "astrocommunity.completion.copilot-lua-cmp" },
+    { import = "astrocommunity.completion.copilot-lua" },
+    {
+        "copilot.lua",
+        opts = {
+            suggestion = {
+                keymap = {
+                    accept = "<C-l>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<C-.>",
+                    prev = "<C-,>",
+                    dismiss = "<C/>",
+                },
+            },
+        }
+    },
     { import = "astrocommunity/debugging/nvim-bqf" },
     { import = "astrocommunity/pack/go" },
     { import = "astrocommunity/pack/typescript" },
