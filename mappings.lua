f1ebbc062736d95eb01098fad2d57bd66e41f463
lcg <lcg635@qq.com>
2023-05-08 11:57:58 +0800

    update

--- mappings.lua
+++ mappings.lua
@@ -17,8 +17,8 @@ return {
     [",c"] = { "*Ncgn", desc = "Replace same word" },
     [",g"] = { function() utils.toggle_term_cmd "lazygit" end, desc = "ToggleTerm lazygit" },
     [",f"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
-    [",w"] = { function() require("spectre").open() end, desc = "Spectre" },
-    [",s"] = { "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", desc = "Spectre (current word)" },
+    [",s"] = { function() require("spectre").open() end, desc = "Spectre" },
+    [",w"] = { "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", desc = "Spectre (current word)" },
 
     ["<F7>"] = false,
     ["<F11>"] = { "<cmd>1ToggleTerm<cr>", desc = "Toggle terminal" },
